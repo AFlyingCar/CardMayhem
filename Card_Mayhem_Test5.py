e4057e0cdeb6a676f88b04e7bb45b9b600f3946c
Tyler Robbins <tyler@familyrobbins.com>
2014-01-16 16:28:34 -0800

    Completed HELP command

--- Card_Mayhem_Test5.py
+++ Card_Mayhem_Test5.py
@@ -11,13 +11,12 @@ print 'Initializing Card Mayhem', '.', '.', '.'
 
 import random
 import time
-import sys
-import pdb
+import sys, pdb
 
 cardValues = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 Cards = ['a', '2','3','4','5','6','7','8','9','10','j','q','k']
 Suits = ['hearts', 'spades', 'diamonds', 'clubs']
-Wild = ['Python', 'X2Multiplier', 'MasterSpark', 'AceTo11', 'DeckSwap']
+Wild = ['X2Multiplier', 'MasterSpark', 'AceTo11', 'DeckSwap']
 Deck = []
 playerDeck = []
 cpuDeck = []
@@ -34,29 +33,20 @@ flipcheck = 1
 
 def shuffle(): #makes main deck
 	foo = 0
-	time.sleep(1)
-	print 'Making Deck'
+	time.sleep(1);print 'Making Deck'
 	newdeck = []
-	while len(newdeck) < 52: #makes card
-		tempDeck = [random.choice(Suits), random.choice(cardValues)]
 
+	while len(newdeck) < 56: #makes card deck
+		if random.randint(1,2) == 1: #chooses
+			tempDeck = [random.choice(Suits), random.choice(cardValues)]
+		else:
+			tempDeck = random.choice(Wild)
+		
 		if tempDeck not in newdeck: #puts card in newdeck
-			newdeck += [tempDeck]
+			newdeck.append(tempDeck)
 	
-	tempDeck = random.choice(Wild)
+	random.shuffle(newdeck) #Note: Look at C:/Users/Tyler/Dropbox/FamilyRobbins/Tyler/usbstuff/Python/yearone/
 	
-	if tempDeck not in newdeck:
-		newdeck += [tempDeck]
-#	print 'Setting Wild Cards'
-
-#	while foo < 4:
-#		if random.randint(1,4) == 1:
-#			tempHand = [random.choice(Wild)]
-
-#			if tempHand not in newdeck:
-#				newdeck += tempHand
-#				foo += 1
-
 	return newdeck #Deck=shuffle()
 
 def deck_maker(run, message): #makes player and cpu decks
@@ -72,7 +62,7 @@ def deck_maker(run, message): #makes player and cpu decks
 		if tempHand in Deck: #removes card from Deck
 			Deck.remove(tempHand)
 
-		deckchoice += [tempHand] #adds card to player/cpu hand
+		deckchoice.append(tempHand) #adds card to player/cpu hand
 
 	return deckchoice #player/cpuDeck=deck_maker(5)
 
@@ -116,12 +106,13 @@ def flipper(deck, message): #flips card in player hand
 			if deck == playerDeck:
 				playerScore -= 10
 				print 'MasterSpark!'
+				playerDisplayed.remove('MasterSpark')
 			elif deck == cpuDeck:
 				playerScore -= 10
 				print 'MasterSpark!'
+				cpuDisplayed.remove('MasterSpark')
 
-
-	return [[tempHand], deck] #returns two values. use arrays to get correct values with tempGrab[]
+	return [tempHand, deck] #returns two values. use arrays to get correct values with tempGrab[]
 
 def AT11(deck):
 	#AceTo11 Wild Card
@@ -130,7 +121,6 @@ def AT11(deck):
 	
 	for item in deck:
 		if item[1] == 1:
-
 			loc = deck.index(item)
 			item[1] = 11
 			deck[loc] = list(item)
@@ -160,8 +150,8 @@ def DeSw(deck):
 		elif swap == 'D': #What to do if player chooses 'D'
 			tempDeck = playerDeck
 			playerDeck = deck_maker(5, 'Rebuilding playerDeck')
-			Deck += tempDeck
-			Deck = random.shuffle(Deck)
+			Deck.append(tempDeck)
+			random.shuffle(Deck)
 			
 			return deck
 
@@ -176,33 +166,18 @@ while restart: #loop to allow the player can restart the game
 	cpuDisplayed = []
 
 	Deck = shuffle()
-
+	print Deck
 	playerDeck = deck_maker(5, 'Building Player/CPU Decks\n')
 	cpuDeck = deck_maker(5, '',)
 
 	while playerNumTurn > 0 and cpuNumTurn > 0 and not finish: #main game loop
 		flipcheck = 0
+		deckTop = Deck[0]
 
-		while flipcheck < 5: #calculates player and cpu scores
-			#breaks out of loop if it catches an IndexError 5 times
-			try:
-				if 'X2Multiplier' in playerDisplayed:
-					playerScore = (sum(map(lambda x: x[1], playerDisplayed))) * 2
-				elif 'X2Multiplier' in cpuDisplayed:
-					cpuScore = (sum(map(lambda x: x[1], cpuDisplayed))) * 2
-				else:
-					playerScore = sum(map(lambda x: x[1], playerDisplayed))
-					cpuScore = sum(map(lambda x: x[1], cpuDisplayed))
-
-				flipcheck = 6
-
-			except(IndexError):
-				flipcheck += 1
-
-		if 'DeckSwap' in cpuDeck:
-			cpuDeck.remove('DeckSwap')
+		if 'DeckSwap' in cpuDeck: #Remove the DeckSwap WildCard from the cpuDeck
+			cpuDeck.remove('DeckSwap') #CPU not smart enough to properly use it
 			tempGrab = flipper(Deck, '')
-			cpuDeck += tempGrab[0]
+			cpuDeck.append(tempGrab[0])
 			Deck = tempGrab[1]
 
 		while playerTurn:
@@ -210,20 +185,21 @@ while restart: #loop to allow the player can restart the game
 			print "You have", len(playerDeck), "cards unflipped, and", len(playerDisplayed), "cards flipped."
 			print "The other player has", len(cpuDeck), "cards unflipped, and", len(cpuDisplayed), "cards flipped.\n"
 
-			playerChoice = raw_input("Type 'F' to flip a card, 'D' to draw a card, and 'E', to exit. ") #tells the player what to do
+			playerChoice = raw_input("Type 'F' to flip a card, 'D' to draw a card, 'E' to exit, or 'HELP' for help. ") #tells the player what to do
 			playerChoice = playerChoice.upper()
 
 			if playerChoice == 'F': #flip a card in the player's hand
 				tempGrab = flipper(playerDeck, 'Flipping card')
 				playerDeck = tempGrab[1]
-				playerDisplayed += (tempGrab[0])
+				playerDisplayed.append(tempGrab[0])
+				print playerDisplayed
 				playerNumTurn -= 1
 				playerTurn = False
 				cpuTurn = True
 
 			elif playerChoice == 'D': #draw a card from the main Deck
 				tempGrab = flipper(Deck, 'Drawing card from main Deck')
-				playerDisplayed += tempGrab[0]
+				playerDisplayed.append(tempGrab[0])
 				Deck = tempGrab[1]
 				playerNumTurn -= 1
 				playerTurn = False
@@ -248,7 +224,13 @@ while restart: #loop to allow the player can restart the game
 						print "'Y' or 'N' please!"
 
 			elif playerChoice == 'HELP':
-				print """
+				help = open("HELP.md", "r")
+				print(help.read())
+				help.close()
+				help = raw_input("Press 1 to show additional commands, and 2 to return to the game: ")
+
+				if help == 1:
+					print """
 >>> 'F'			---->	Flips a card in your deck.
 >>> 'D'			---->	Draws a card from the main deck.
 >>> 'E'			---->	Exits from the game.
@@ -258,9 +240,10 @@ while restart: #loop to allow the player can restart the game
 >>> 'PDISP'		---->	Shows all cards in the player's hand.
 >>> 'CDISP'		---->	Shows all cards in the cpu's hand.
 >>> 'ADD'		---->	Adds a specified card.
->>> 'TURNRESET'	---->	Resets player and cpu turns. Has the same effect of restarting the game
->>> 'TRACE'		---->	Actiivates pdb debugging traceback.
+>>> 'TURNRESET'		---->	Resets player and cpu turns. Has the same effect of restarting the game
+>>> 'TRACE'		---->	Actiivates the pdb debugging traceback.
 				"""
+					raw_input("Press any key to continue: ")
 
 			#Cheat codes for debugging
 			elif playerChoice == 'DECK':
@@ -278,7 +261,7 @@ while restart: #loop to allow the player can restart the game
 				choice = [raw_input(">>> Enter the card to add(Example: hearts, 3): ")]
 				tempHand = choice.split()
 				if choice in str(Deck):
-					playerDisplayed += [choice]
+					playerDisplayed.append(choice)
 					Deck.remove(choice)
 				elif choice not in str(Deck):
 					print "Could not find" + choice + "in Deck."
@@ -323,10 +306,27 @@ while restart: #loop to allow the player can restart the game
 			else: #if cpu decides to draw a card
 				tempGrab = flipper(Deck, 'Drawing card from main Deck')
 				Deck = tempGrab[1]
-				cpuDisplayed += tempGrab[0]
+				cpuDisplayed.append(tempGrab[0])
 				cpuNumTurn -= 1
 				cpuTurn = False
 				playerTurn = True
+
+		while flipcheck < 5: #calculates player and cpu scores
+			#breaks out of loop if it catches an IndexError 5 times
+			try:
+				if 'X2Multiplier' in playerDisplayed:
+					playerScore = (sum(map(lambda x: x[1], playerDisplayed))) * 2
+				elif 'X2Multiplier' in cpuDisplayed:
+					cpuScore = (sum(map(lambda x: x[1], cpuDisplayed))) * 2
+				else:
+					playerScore = sum(map(lambda x: x[1], playerDisplayed))
+					
+					cpuScore = sum(map(lambda x: x[1], cpuDisplayed))
+
+				flipcheck = 6
+
+			except(IndexError, TypeError):
+				flipcheck += 1
 
 	if playerNumTurn <= 0 and cpuNumTurn <= 0:
 		if playerScore > cpuScore:
